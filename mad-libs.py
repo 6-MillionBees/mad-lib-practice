807eacf45066f9879c68e91bab2ec5216245aad8
6MillionBees <BoettcherAr321@northwested.org>
2024-09-16 10:04:52 -0400

    Update mad-libs.py

--- mad-libs.py
+++ mad-libs.py
@@ -2,8 +2,8 @@
 # 9/16/24
 # Mad Libs
 
-color = input('Give me a color: ')
 adjective1 = input('Give me an adjective: ')
+color = input('Give me a color: ')
 time = input('Give me a time: ')
 adjective2 = input('Give me another adjective: ')
 place = input('Give me a place: ')
@@ -13,9 +13,9 @@ verb = input('Give me a verb: ')
 noun = input('Give me a noun: ')
 number = input('Give me a number: ')
 
-print(f'Bats are so cool! They are {color}, {adjective1} animals which have wings.') # it was too long so I split it up onto multiple lines
-print(f'They like to fly around at {time} which makes some people scared of them.')
-print(f'But bats are {adjective2}, and they don\'t want to hurt people. ')
-print(f'I have a pet bat that lives in {place}. I like to feed him {food1} and {food2}. ')
-print(f'He likes to {verb}. I am his favorite person, but he also likes {noun}. ')
-print(f'I want to convince my parents to get me {number} more bats.')
+print(f'BATS ARE SO {adjective1.upper()}! They are {color.lower()} animals which have wings.') # it was too long so I split it up onto multiple lines
+print(f'They like to fly around at {time} which makes some people scared of them.') # the reason there are so many .lower() 
+print(f'But bats are {adjective2.lower()}, and they don\'t want to hurt people. ')
+print(f'I have a pet bat that lives in {place.lower()}. I like to feed him {food1.lower()} and {food2.lower()}. ')
+print(f'He likes to {verb.lower()}. I am his favorite person, but he also likes {noun}. ')
+print(f'I want to convince my parents to get me {number.lower()} more bats.')
