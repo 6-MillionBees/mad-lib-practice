4db9e0bb53755116658879877d3da73c904f0d39
6MillionBees <BoettcherAr321@northwested.org>
2024-09-16 10:05:14 -0400

    Update mad-libs.py

--- mad-libs.py
+++ mad-libs.py
@@ -14,7 +14,7 @@ noun = input('Give me a noun: ')
 number = input('Give me a number: ')
 
 print(f'BATS ARE SO {adjective1.upper()}! They are {color.lower()} animals which have wings.') # it was too long so I split it up onto multiple lines
-print(f'They like to fly around at {time} which makes some people scared of them.') # the reason there are so many .lower() 
+print(f'They like to fly around at {time} which makes some people scared of them.')
 print(f'But bats are {adjective2.lower()}, and they don\'t want to hurt people. ')
 print(f'I have a pet bat that lives in {place.lower()}. I like to feed him {food1.lower()} and {food2.lower()}. ')
 print(f'He likes to {verb.lower()}. I am his favorite person, but he also likes {noun}. ')
