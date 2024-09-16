bf0c2ccdf4abc2ba6613a723c6316e92e96a5f7c
6MillionBees <BoettcherAr321@northwested.org>
2024-09-16 09:22:45 -0400

    Add files via upload

---
+++ mad-libs.py
@@ -0,0 +1,21 @@
+# Arden Boettcher
+# 9/16/24
+# Mad Libs
+
+color = input('Give me a color: ')
+adjective1 = input('Give me an adjective: ')
+time = input('Give me a time: ')
+adjective2 = input('Give me another adjective: ')
+place = input('Give me a place: ')
+food1 = input('Give me a food: ')
+food2 = input('Give me another food: ')
+verb = input('Give me a verb: ')
+noun = input('Give me a noun: ')
+number = input('Give me a number: ')
+
+print(f'Bats are so cool! They are {color}, {adjective1} animals which have wings.') # it was too long so I split it up onto multiple lines
+print(f'They like to fly around at {time} which makes some people scared of them.')
+print(f'But bats are {adjective2}, and they don\'t want to hurt people. ')
+print(f'I have a pet bat that lives in {place}. I like to feed him {food1} and {food2}. ')
+print(f'He likes to {verb}. I am his favorite person, but he also likes {noun}. ')
+print(f'I want to convince my parents to get me {number} more bats.')
